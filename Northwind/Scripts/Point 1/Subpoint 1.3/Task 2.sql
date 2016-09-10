b862e1d9768f586e63e57a1237e273b4b0e49b28
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-09-11 01:21:33 +0300

    Tasks from point 1, subpoint 1.3

---
+++ Northwind/Scripts/Point 1/Subpoint 1.3/Task 2.sql
@@ -0,0 +1,11 @@
+﻿-- Выбрать всех заказчиков из таблицы Customers, у которых название страны
+-- начинается на буквы из диапазона b и g. Использовать оператор BETWEEN.
+-- Проверить, что в результаты запроса попадает Germany. Запрос должен возвращать
+-- только колонки CustomerID и Country и отсортирован по Country
+
+SELECT
+  CustomerID,
+  Country
+FROM Customers
+WHERE LEFT(Country, 1) BETWEEN 'B' AND 'G'
+ORDER BY Country
---
+++ Northwind/Scripts/Point 1/Subpoint 1.3/Task 3.sql
@@ -0,0 +1,10 @@
+﻿-- Выбрать всех заказчиков из таблицы Customers, у которых название страны
+-- начинается на буквы из диапазона b и g, не используя оператор BETWEEN
+
+SELECT
+  CustomerID,
+  Country
+FROM Customers
+WHERE LEFT(Country, 1) >= 'B' AND LEFT(Country, 1) <= 'G'
+--WHERE LEFT(Country, 1) IN ('B', 'C', 'D', 'E', 'F', 'G')
+ORDER BY Country
