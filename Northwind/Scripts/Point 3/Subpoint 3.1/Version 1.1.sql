db14d4b5a5c13ece665d5f562c84d80039ea6ad4
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-09-11 23:47:55 +0300

    Alter Scripts (point 3, subpoint 3.1)

---
+++ Northwind/Scripts/Point 3/Subpoint 3.1/Version 1.1.sql
@@ -0,0 +1,26 @@
+﻿IF NOT EXISTS
+(
+  SELECT
+    *
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_NAME = 'CreditCards'
+)
+BEGIN
+  CREATE TABLE "CreditCards" (
+    "CardID" "int" IDENTITY (1, 1) NOT NULL ,
+    "CardNo" nvarchar (19) NOT NULL ,
+    "EmployeeID" "int" NOT NULL ,
+    "ValidThrough" "datetime" NOT NULL ,
+    "CardHolder" nvarchar (31) NOT NULL ,
+    CONSTRAINT "PK_CreditCards" PRIMARY KEY  CLUSTERED 
+    (
+    	"CardID"
+    ),
+    CONSTRAINT "FK_CreditCards_Employees" FOREIGN KEY 
+    (
+    	"EmployeeID"
+    ) REFERENCES "dbo"."Employees" (
+    	"EmployeeID"
+    )
+  )
+END
---
+++ Northwind/Scripts/Point 3/Subpoint 3.1/Version 1.3.sql
@@ -0,0 +1,23 @@
+﻿IF EXISTS
+(
+  SELECT
+    *
+  FROM INFORMATION_SCHEMA.TABLES
+  WHERE TABLE_NAME = 'Region'
+)
+BEGIN
+  EXEC sp_rename 'Region', 'Regions'
+END
+
+IF NOT EXISTS
+(
+  SELECT
+    *
+  FROM sys.columns 
+  WHERE Name = 'FoundedDate'
+    AND Object_ID = Object_ID('Customers')
+)
+BEGIN
+  ALTER TABLE Customers
+  ADD FoundedDate DATETIME NULL
+END
