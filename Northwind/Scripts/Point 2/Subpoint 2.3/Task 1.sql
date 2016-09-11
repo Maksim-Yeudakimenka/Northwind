918f1b6fb08bc20915580a95e75a5db3040fb1ce
Maksim Yeudakimenka <Maksim_Yeudakimenka@epam.com>
2016-09-11 22:17:24 +0300

    Tasks from point 2, subpoint 2.3

---
+++ Northwind/Scripts/Point 2/Subpoint 2.3/Task 1.sql
@@ -0,0 +1,13 @@
+﻿-- Определить продавцов, которые обслуживают регион 'Western' (таблица Region)
+
+SELECT DISTINCT
+  E.LastName + ' ' + E.FirstName AS Seller,
+  R.RegionDescription AS Region
+FROM Employees AS E
+  INNER JOIN EmployeeTerritories AS ET
+    ON E.EmployeeID = ET.EmployeeID
+  INNER JOIN Territories AS T
+    ON ET.TerritoryID = T.TerritoryID
+  INNER JOIN Region AS R
+    ON T.RegionID = R.RegionID
+WHERE R.RegionDescription = 'Western'
---
+++ Northwind/Scripts/Point 2/Subpoint 2.3/Task 2.sql
@@ -0,0 +1,13 @@
+﻿-- Выдать в результатах запроса имена всех заказчиков из таблицы Customers
+-- и суммарное количество их заказов из таблицы Orders. Принять во внимание,
+-- что у некоторых заказчиков нет заказов, но они также должны быть выведены в результатах запроса.
+-- Упорядочить результаты запроса по возрастанию количества заказов
+
+SELECT
+  C.ContactName,
+  COUNT(O.OrderID) AS 'Total Orders'
+FROM Customers AS C
+  LEFT OUTER JOIN Orders AS O
+    ON C.CustomerID = O.CustomerID
+GROUP BY C.ContactName
+ORDER BY 'Total Orders'
