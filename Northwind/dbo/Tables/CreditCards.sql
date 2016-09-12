2d8f4b24c4de3f56c514f35ec62053284dde7888
Maksim Yeudakimenka <Maksim_Yeudakimenka@epam.com>
2016-09-12 12:10:47 +0300

    SSDT Deployment Version 1.1 (point 3, subpoint 3.2)

---
+++ Northwind/dbo/Tables/CreditCards.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[CreditCards]
+(
+    [CardID] INT IDENTITY(1, 1) NOT NULL, 
+    [CardNo] NVARCHAR(19) NOT NULL, 
+    [EmployeeID] INT NOT NULL, 
+    [ValidThrough] DATETIME NOT NULL, 
+    [CardHolder] NVARCHAR(31) NOT NULL,
+    CONSTRAINT [PK_CreditCards] PRIMARY KEY  CLUSTERED 
+    (
+    	[CardID]
+    ),
+    CONSTRAINT [FK_CreditCards_Employees] FOREIGN KEY 
+    (
+    	[EmployeeID]
+    ) REFERENCES [dbo].[Employees] (
+    	[EmployeeID]
+    )
+)
